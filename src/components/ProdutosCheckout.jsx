3c8520cfa1ed5841384b2de3d6488eed22f3c1c1
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-10-21 14:59:36 -0300

    feat: Novo componente 'ProdutosCheckout'

---
+++ src/components/ProdutosCheckout.jsx
@@ -0,0 +1,20 @@
+import convertaParaReal from "../utils/convertaParaReal";
+
+const ProdutoosCheckout = ({itemDoCarrinho, addShopCart, removeShopCart}) => {    
+    return (
+            <li>
+                <figure><img style={{width : '50px', height: 'auto'}} src={itemDoCarrinho.image} alt={itemDoCarrinho.description} /></figure>
+                <header><h2>{itemDoCarrinho.title}</h2></header>
+                <div><p>{ convertaParaReal(itemDoCarrinho.price)}</p></div>
+                <footer>
+                        <p>Quantidade: {itemDoCarrinho.quantidade}</p>
+                        <div>
+                            <button onClick={ () => removeShopCart(itemDoCarrinho) }>-</button>
+                            <button onClick={ () => addShopCart(itemDoCarrinho) }>+</button>
+                        </div>
+                </footer>
+            </li>
+            )
+}
+
+export default ProdutoosCheckout;
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import ProdutosCarrinho from "../components/ProdutosCarrinho";
+import ProdutoosCheckout from "../components/ProdutosCheckout";
 
 const Checkout = ({arrayCarrinho, addShopCart, removeShopCart}) => {
     const navigate = useNavigate()
@@ -12,7 +12,7 @@ const Checkout = ({arrayCarrinho, addShopCart, removeShopCart}) => {
         <h1>Carrinho de compras</h1>
         <ul>
             {arrayCarrinho.map((itemDoCarrinho, index) => 
-                <ProdutosCarrinho key={index} itemDoCarrinho={itemDoCarrinho} addShopCart={addShopCart} removeShopCart={removeShopCart}/>
+                <ProdutoosCheckout key={index} itemDoCarrinho={itemDoCarrinho} addShopCart={addShopCart} removeShopCart={removeShopCart}/>
             )}
         </ul>
         <div>
