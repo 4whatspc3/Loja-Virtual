f68db4acad6e92cb8fd026a80479a1ea3a7c4a1b
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-10-22 11:05:25 -0300

    fix: Typo - 'ProdutoosCheckout' -> 'ProdutosCheckout'

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import convertaParaReal from "../utils/convertaParaReal";
 
 const Produtos = ({produto, addShopCart}) => {
+    const navigate = useNavigate()
 
+    const handleClick = (id) => {
+        navigate(`/produtos/${id}`, { state : {name : 'joão'} })
+    }
+    
     return (
             <li>
-                <figure><img style={{width : '100px', height: 'auto'}} src={produto.image} alt={produto.description} /></figure>
+                <figure><img onClick={ () => handleClick(produto.id) } style={{width : '100px', height: 'auto'}} src={produto.image} alt={produto.description} /></figure>
                 <header><h2>{produto.title}</h2></header>
                 <div><p>{convertaParaReal(produto.price)}</p></div>
                 <footer>
