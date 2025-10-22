cc72475fa79c11961521919acfbca09ca992163e
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-10-22 11:34:52 -0300

    fix: Typo - 'ProdutoosCheckout' ->  'ProdutosCheckout'

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import ProdutoosCheckout from "../components/ProdutosCheckout";
+import ProdutosCheckout from "../components/ProdutosCheckout";
 
 const Checkout = ({arrayCarrinho, addShopCart, removeShopCart}) => {
     const navigate = useNavigate()
@@ -12,7 +12,7 @@ const Checkout = ({arrayCarrinho, addShopCart, removeShopCart}) => {
         <h1>Carrinho de compras</h1>
         <ul>
             {arrayCarrinho.map((itemDoCarrinho, index) => 
-                <ProdutoosCheckout key={index} itemDoCarrinho={itemDoCarrinho} addShopCart={addShopCart} removeShopCart={removeShopCart}/>
+                <ProdutosCheckout key={index} itemDoCarrinho={itemDoCarrinho} addShopCart={addShopCart} removeShopCart={removeShopCart}/>
             )}
         </ul>
         <div>
