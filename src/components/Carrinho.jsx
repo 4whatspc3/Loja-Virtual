b7118aedcb206672fdf62b412ed89f50c2f4d642
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-10-23 15:51:10 -0300

    feat: Carrinho não é renderizado em 'Home' ou 'Checkout'

--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -3,20 +3,24 @@ import ProdutosCarrinho from "./ProdutosCarrinho"
 
 const Carrinho = ({arrayCarrinho, addShopCart, removeShopCart, valorTotal}) => {
         const navigate = useNavigate()
+        const location = useLocation()
 
         const handleClick = () => {
             navigate('/checkout')
         }
 
-        return(<ul>
-                {arrayCarrinho.map((itemDoCarrinho) => 
-                    <ProdutosCarrinho key={itemDoCarrinho.id} itemDoCarrinho={itemDoCarrinho} addShopCart={addShopCart} removeShopCart={removeShopCart}/>
-                )}
-                <div>
-                    <p>{valorTotal}</p>
-                    <button onClick={ handleClick }>Finalizar a compra</button>
-                </div>
-            </ul>)
+        return(location.pathname === '/' || location.pathname === '/checkout' ?
+                (<></>) :
+                <ul>
+                    {arrayCarrinho.map((itemDoCarrinho) => 
+                        <ProdutosCarrinho key={itemDoCarrinho.id} itemDoCarrinho={itemDoCarrinho} addShopCart={addShopCart} removeShopCart={removeShopCart}/>
+                    )}
+                    <div>
+                        <p>{valorTotal}</p>
+                        <button onClick={ handleClick }>Finalizar a compra</button>
+                    </div>
+                </ul>
+        )
 }
 
 export default Carrinho;
