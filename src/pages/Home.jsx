a9f6d0a3c06ba3daed8685977ae4d07511f68f0b
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-10-23 17:51:43 -0300

    feat: O usuário precisa fazer login em 'Home'
    A estrutura é provisória, serve apenas para exemplo.
    Futuramente adaptar com uso do React Hook Form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
+//Provisório, preferível usar o 'React Hook Form'
 const Home = () => {
-    return <Link to="/produtos">Vá para os produtos</Link>
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [erro, setErro] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        email.length >= 11 && senha.length >= 5 ? navigate('/produtos') : setErro('Preencha os campos corretamente')
+    }
+
+    return (
+        <form onSubmit={ handleSubmit}>
+            <div>
+                <label htmlFor="userEmail">Email:</label>            
+                <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="exemplo@seuemail.com" value={email} id="userEmail"/>
+            </div>
+            <div>
+                <label htmlFor="userSenha">Senha:</label>
+                <input onChange={(e) => setSenha(e.target.value)} type='password' value={senha} id="userSenha"/>
+                <p>A senha precisa de no mínimo 5 caracteres</p>
+            </div>
+            <button >Entrar</button>
+            <p>{erro}</p>
+        </form>
+    )
 }
 
 export default Home;
