2569333938bd6de0847a0d5bc8e3234edc0d5df6
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-10-26 19:30:12 -0300

    style: Estilize 'Hero' para mobile e importe a fonte Inter

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -2,9 +2,16 @@ import imageHero from "../../assets/hero/carrinho.png"
 
 const Hero = () => {
     return (
-        <header className="h-svh bg-sky-50 p-4 m-2 rounded-xl">
-            <h1 className="text-8xl text-sky-700 font-medium">Aqui Tem de Tudo</h1>
-            <figure><img src={imageHero} alt="Carrinho de compras" className="w-full h-auto object-cover mt-18"/></figure>
+        <header className="h-svh bg-sky-50 p-4 m-2 rounded-xl flex flex-col gap-10">
+            <h1 className="text-8xl text-sky-700 font-bold font-inter italic ml-2">
+                Aqui Tem de Tudo
+            </h1>
+            <figure>
+                <img src={imageHero} alt="Carrinho de compras" className="w-full h-auto object-cover"/>
+            </figure>
+            <button className="block mx-auto rounded-2xl bg-white text-2xl font-medium font-inter text-sky-400 border border-sky-400 p-2 pl-4 pr-4">
+                Compre já!
+            </button>
         </header>
     )
 }
