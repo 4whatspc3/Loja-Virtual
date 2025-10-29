d0c2a24c7554b01fdcb76b263013cd35b24bc818
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-10-29 12:10:52 -0300

    style: Estilize o carrinho de compras para mobile

--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -11,15 +11,18 @@ const Carrinho = ({arrayCarrinho, addShopCart, removeShopCart, valorTotal}) => {
 
         return(location.pathname === '/' || location.pathname === '/checkout' ?
                 (<></>) :
-                <ul>
-                    {arrayCarrinho.map((itemDoCarrinho) => 
-                        <ProdutosCarrinho key={itemDoCarrinho.id} itemDoCarrinho={itemDoCarrinho} addShopCart={addShopCart} removeShopCart={removeShopCart}/>
-                    )}
-                    <div>
-                        <p>{valorTotal}</p>
-                        <button onClick={ handleClick }>Finalizar a compra</button>
+                <details className="fixed right-0 flex flex-col gap-4 p-4 overflow-y-auto">
+                    <summary className="bg-white/80 text-sky-700 font-medium rounded-4xl border border-sky-300 p-2">Carrinho</summary>
+                    <ul className="flex flex-col gap-4 bg-white/80 p-4 mb-4 w-full max-w-80 h-60 overflow-auto rounded-4xl border border-sky-300">
+                        {arrayCarrinho.map((itemDoCarrinho) => 
+                            <ProdutosCarrinho key={itemDoCarrinho.id} itemDoCarrinho={itemDoCarrinho} addShopCart={addShopCart} removeShopCart={removeShopCart}/>
+                        )}
+                    </ul>
+                    <div className="flex gap-4 bg-white/80 p-4 border border-sky-300 rounded-4xl justify-between items-center">
+                        <p className="font-medium font-inter text-sky-800">Total: {valorTotal}</p>
+                        <button onClick={ handleClick } className="p-2 rounded-lg border border-green-500 text-green-500">Finalizar a compra</button>
                     </div>
-                </ul>
+                </details>
         )
 }
 
--- src/components/ProdutosCarrinho.jsx
+++ src/components/ProdutosCarrinho.jsx
@@ -3,15 +3,21 @@ import convertaParaReal from "../utils/convertaParaReal";
 
 const ProdutosCarrinho = ({itemDoCarrinho, addShopCart, removeShopCart}) => {    
     return (
-            <li>
-                <figure><img style={{width : '50px', height: 'auto'}} src={itemDoCarrinho.image} alt={itemDoCarrinho.description} /></figure>
-                <header><h2>{itemDoCarrinho.title}</h2></header>
-                <div><p>{ convertaParaReal(itemDoCarrinho.price)}</p></div>
-                <footer>
-                        <p>Quantidade: {itemDoCarrinho.quantidade}</p>
-                        <div>
-                            <button onClick={ () => removeShopCart(itemDoCarrinho) }>-</button>
-                            <button onClick={ () => addShopCart(itemDoCarrinho) }>+</button>
+            <li className="flex flex-col gap-4 rounded-4xl rounded-b-none p-4 pt-0 border-b-2 border-sky-700">
+                <header className="flex gap-2">
+                    <figure className="w-full p-2 rounded-2xl">
+                        <img style={{width : '50px', height: 'auto'}} src={itemDoCarrinho.image} alt={itemDoCarrinho.description} className="mx-auto w-40 h-40 object-contain"/>
+                    </figure>
+                    <h2 className="font-medium font-inter text-sky-800 ">{itemDoCarrinho.title}</h2>
+                </header>
+                <footer className="flex gap-4">
+                        <div className="flex flex-col justify-between font-medium font-inter text-sky-800">
+                            <p>Quantidade: {itemDoCarrinho.quantidade}</p>
+                            <p className="italic">Unidade:{ convertaParaReal(itemDoCarrinho.price)}</p>
+                        </div>
+                        <div className="flex gap-4 ml-auto">
+                            <button onClick={ () => removeShopCart(itemDoCarrinho) } className="w-10 h-10 pl-2 pr-2 rounded-lg border border-red-500 text-red-500">-</button>
+                            <button onClick={ () => addShopCart(itemDoCarrinho) } className="w-10 h-10 pl-2 pr-2 rounded-lg border border-green-500 text-green-500">+</button>
                         </div>
                 </footer>
             </li>
