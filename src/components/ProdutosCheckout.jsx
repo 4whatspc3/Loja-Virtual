ac051ba9c9eb846d39eb0d0b110df0de8ae500c9
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-11-01 14:31:51 -0300

    style: Estilize o componente 'ProdutosCheckout'

--- src/components/ProdutosCheckout.jsx
+++ src/components/ProdutosCheckout.jsx
@@ -2,16 +2,20 @@ import convertaParaReal from "../utils/convertaParaReal";
 
 const ProdutosCheckout = ({itemDoCarrinho, addShopCart, removeShopCart}) => {    
     return (
-            <li>
-                <figure><img style={{width : '50px', height: 'auto'}} src={itemDoCarrinho.image} alt={itemDoCarrinho.description} /></figure>
-                <header><h2>{itemDoCarrinho.title}</h2></header>
-                <div><p>{ convertaParaReal(itemDoCarrinho.price)}</p></div>
-                <footer>
-                        <p>Quantidade: {itemDoCarrinho.quantidade}</p>
-                        <div>
-                            <button onClick={ () => removeShopCart(itemDoCarrinho) }>-</button>
-                            <button onClick={ () => addShopCart(itemDoCarrinho) }>+</button>
-                        </div>
+            <li className="w-full max-w-200 mx-auto flex flex-col gap-8 rounded-4xl rounded-b-none p-4 pt-0 border-b-2 border-sky-700">
+                <header className="flex flex-col gap-6">
+                    <figure className="w-full p-2 rounded-2xl"><img src={itemDoCarrinho.image} alt={itemDoCarrinho.description} className="mx-auto w-40 h-40 object-contain"/></figure>
+                    <h2 className="font-medium font-inter text-sky-800 ">{itemDoCarrinho.title}</h2>
+                </header>
+                <footer className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-2 justify-between font-medium font-inter text-sky-800">
+                        <p >Quantidade: {itemDoCarrinho.quantidade}</p>
+                        <p className="italic">Unidade: { convertaParaReal(itemDoCarrinho.price)}</p>
+                    </div>
+                    <div className="flex gap-4 ml-auto">
+                        <button onClick={ () => removeShopCart(itemDoCarrinho) } className="w-10 h-10 pl-2 pr-2 rounded-lg border border-red-500 text-red-500">-</button>
+                        <button onClick={ () => addShopCart(itemDoCarrinho) } className="w-10 h-10 pl-2 pr-2 rounded-lg border border-green-500 text-green-500">+</button>
+                    </div>
                 </footer>
             </li>
             )
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -8,14 +8,14 @@ const Checkout = ({arrayCarrinho, addShopCart, removeShopCart}) => {
         navigate(-1)
     }
 
-    return (<div className="p-6 flex flex-col gap-6">
-        <h1 className="text-3xl font-inter font-medium text-sky-700">Carrinho de compras</h1>
+    return (<div className="p-6 flex flex-col gap-20">
+        <h1 className="mx-auto text-3xl font-inter font-medium text-sky-700">Carrinho de compras</h1>
         <ul className="flex flex-col gap-4">
             {arrayCarrinho.map((itemDoCarrinho, index) => 
                 <ProdutosCheckout key={index} itemDoCarrinho={itemDoCarrinho} addShopCart={addShopCart} removeShopCart={removeShopCart}/>
             )}
         </ul>
-        <div>
+        <div className="mx-auto">
             <button onClick={ handleClick } className="bg-sky-300 border border-sky-200 p-2 rounded-2xl font-inter font-medium text-white text-xl">Voltar</button>
         </div>
     </div>)
