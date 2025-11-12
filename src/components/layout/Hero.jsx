2075e66df634731b14126afa474dfc7290e45cad
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-11-12 12:24:26 -0300

    feat: Novo layout e imagens para o Hero (3 novas imagens)

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,24 +1,28 @@
-import imageHero from "../../assets/hero/carrinho.png"
+import imageD from "../../assets/hero/d.jpg"
+import imageB from "../../assets/hero/b.jpg"
+import imageC from "../../assets/hero/c.jpg"
+
 
 const Hero = () => {
     return (
-        <header className="h-svh min-h-200 bg-sky-50 p-4 m-2 rounded-xl flex flex-col items-center gap-10
-                           sm:p-8 sm:gap-30 md:pt-12 xl:flex-row xl:justify-center"> 
-            <div className="xl:flex xl:flex-col xl:gap-40">
-                <h1 className="w-full max-w-120 text-center text-8xl text-sky-700 font-bold font-inter italic xl:max-w-full xl:text-9xl">
-                    Aqui Tem <br className="hidden xl:inline"/>de Tudo
+        <header className="h-svh min-h-205 w-full flex flex-col overflow-hidden">
+            <div className="flex items-center justify-center p-6 sm:p-8 w-full h-full min-h-80 bg-linear-to-b from-sky-300 via-sky-300 to-white">
+                <h1 className="text-center text-white text-7xl sm:text-8xl xl:text-9xl font-bold font-inter italic">
+                    Aqui Tem de Tudo!
                 </h1>
-                <button className="hidden xl:block mx-auto rounded-2xl bg-white text-2xl font-medium font-inter text-sky-400 border border-sky-400 p-2 pl-4 pr-4">
-                    Compre já!
-                </button>
             </div>
-            <div className="w-full flex flex-col items-center gap-10 md:flex-row md:justify-between md:max-w-220 xl:justify-center">
-                <figure className="w-full max-w-100 md:max-w-120">
-                    <img src={imageHero} alt="Carrinho de compras" className="w-full h-auto object-cover"/>
-                </figure>
-                <button className="block mx-auto rounded-2xl bg-white text-2xl font-medium font-inter text-sky-400 border border-sky-400 p-2 pl-4 pr-4 md:mx-0 xl:hidden">
-                    Compre já!
-                </button>
+            <div className="w-full h-2/3 sm:h-full flex relative">
+                <div className="w-full h-100 bg-linear-to-b from-white via-white/10 to-transparent absolute">
+                </div>
+                <div className="w-full overflow-hidden hidden md:block">
+                    <img src={imageD} alt="" className="w-full object-cover 2xl:object-[50%_-200px] "/>
+                </div>
+                <div className="w-full overflow-hidden hidden sm:block">
+                    <img src={imageB} alt="" className="w-full object-cover"/>
+                </div>
+                <div className="w-full overflow-hidden">
+                    <img src={imageC} alt="" className="w-full object-cover"/>
+                </div>
             </div>
         </header>
     )
